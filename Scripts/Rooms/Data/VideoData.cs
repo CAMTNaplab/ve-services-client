7aa04b6558a2caa0e2b960e694be27666a583a4d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-12-21 10:46:13 +0700

    Implement media service API client

---
+++ Scripts/Rooms/Data/VideoData.cs
@@ -0,0 +1,12 @@
+namespace VEServicesClient
+{
+    [System.Serializable]
+    public struct VideoData
+    {
+        public string id;
+        public string playListId;
+        public string filePath;
+        public float duration;
+        public int sortOrder;
+    }
+}
---
+++ Scripts/Services/MediaService.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityRestClient;
+
+namespace VEServicesClient
+{
+    public class MediaService
+    {
+        public static async Task<RestClient.Result<ClientData>> AddUser(string userToken)
+        {
+            Dictionary<string, object> formData = new Dictionary<string, object>();
+            formData["userToken"] = userToken;
+            return await RestClient.Post<Dictionary<string, object>, ClientData>(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/add-user"), formData);
+        }
+
+        public static async Task<RestClient.Result> RemoveUser(string userToken)
+        {
+            Dictionary<string, object> formData = new Dictionary<string, object>();
+            formData["userToken"] = userToken;
+            return await RestClient.Post(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/remove-user"), formData);
+        }
+
+        public static async Task<RestClient.Result> DeleteMedia(string id)
+        {
+            return await RestClient.Delete(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/" + id));
+        }
+
+        public static async Task<RestClient.Result<List<VideoData>>> GetVideos(string playListId)
+        {
+            return await RestClient.Get<List<VideoData>>(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/" + playListId));
+        }
+    }
+}
