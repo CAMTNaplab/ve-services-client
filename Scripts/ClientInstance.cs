fd04e740b96cf973c37203f4c425805e17dc557f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-12-26 16:50:39 +0700

    WIP room connection implementation

--- Scripts/ClientInstance.cs
+++ Scripts/ClientInstance.cs
@@ -1,6 +1,4 @@
-using Colyseus;
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace VEServicesClient
@@ -13,15 +11,10 @@ namespace VEServicesClient
         public bool secured = false;
         public string secret = "secret";
 
-        private ColyseusClient _client;
-        public ColyseusClient Client
-        {
-            get
-            {
-                _client.Settings.useSecureProtocol = secured;
-                return _client;
-            }
-        }
+        public BroadcastRoom BroadcastRoom { get; private set; } = null;
+        public ChatRoom ChatRoom { get; private set; } = null;
+        public ListingRoom ListingRoom { get; private set; } = null;
+        public MediaRoom MediaRoom { get; private set; } = null;
 
         private void Awake()
         {
@@ -34,9 +27,62 @@ namespace VEServicesClient
             DontDestroyOnLoad(gameObject);
         }
 
+        public string GetWsAddress()
+        {
+            return (secured ? "wss://" : "ws://") + address;
+        }
+
         public string GetApiAddress()
         {
             return (secured ? "https://" : "http://") + address;
+        }
+
+        public async Task<BroadcastRoom> JoinBroadcastRoom()
+        {
+            if (BroadcastRoom != null)
+                await BroadcastRoom.Leave();
+            BroadcastRoom = new BroadcastRoom();
+            if (await BroadcastRoom.Join())
+            {
+                return BroadcastRoom;
+            }
+            return null;
+        }
+
+        public async Task<ChatRoom> JoinChatRoom()
+        {
+            if (ChatRoom != null)
+                await ChatRoom.Leave();
+            ChatRoom = new ChatRoom();
+            if (await ChatRoom.Join())
+            {
+                return ChatRoom;
+            }
+            return null;
+        }
+
+        public async Task<ListingRoom> JoinListingRoom(GameServerData data)
+        {
+            if (ListingRoom != null)
+                await ListingRoom.Leave();
+            ListingRoom = new ListingRoom(data);
+            if (await ListingRoom.Join())
+            {
+                return ListingRoom;
+            }
+            return null;
+        }
+
+        public async Task<MediaRoom> JoinMediaRoom()
+        {
+            if (MediaRoom != null)
+                await MediaRoom.Leave();
+            MediaRoom = new MediaRoom();
+            if (await MediaRoom.Join())
+            {
+                return MediaRoom;
+            }
+            return null;
         }
     }
 }
