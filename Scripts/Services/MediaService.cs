1a717018e7b028d543518ce0c098d65201043435
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-12-29 13:34:24 +0700

    Apply server secret and implement upload function

--- Scripts/Services/MediaService.cs
+++ Scripts/Services/MediaService.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
 using UnityRestClient;
 
 namespace VEServicesClient
@@ -11,24 +13,69 @@ namespace VEServicesClient
         {
             Dictionary<string, object> formData = new Dictionary<string, object>();
             formData["userId"] = userId;
-            return await RestClient.Post<Dictionary<string, object>, ClientData>(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/add-user"), formData);
+            return await RestClient.Post<Dictionary<string, object>, ClientData>(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/add-user"), formData, ClientInstance.Instance.secret);
         }
 
         public static async Task<RestClient.Result> RemoveUser(string userId)
         {
             Dictionary<string, object> formData = new Dictionary<string, object>();
             formData["userId"] = userId;
-            return await RestClient.Post(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/remove-user"), formData);
+            return await RestClient.Post(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/remove-user"), formData, ClientInstance.Instance.secret);
         }
 
-        public static async Task<RestClient.Result> DeleteMedia(string id)
+        public static async Task<RestClient.Result> DeleteMedia(string token, string id)
         {
-            return await RestClient.Delete(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/" + id), string.Empty);
+            return await RestClient.Delete(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/" + id), token);
         }
 
         public static async Task<RestClient.Result<List<VideoData>>> GetVideos(string playListId)
         {
             return await RestClient.Get<List<VideoData>>(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/media/" + playListId));
+        }
+
+        public static async Task<RestClient.Result> UploadMedia(string token, string playListId, byte[] file, string fileExt, System.Action<float> onProgress = null, System.Action onDone = null)
+        {
+            WWWForm form = new WWWForm();
+            if (fileExt.Equals("mp4"))
+                form.AddBinaryData("file", file, "file.mp4", "video/mp4");
+            else if (fileExt.Equals("wav"))
+                form.AddBinaryData("file", file, "file.wav", "audio/x-wav");
+            form.AddField(nameof(playListId), playListId);
+
+            UnityWebRequest webRequest = UnityWebRequest.Post(RestClient.GetUrl(ClientInstance.Instance.GetApiAddress(), "/upload"), form);
+            webRequest.certificateHandler = new SimpleWebRequestCert();
+            webRequest.SetRequestHeader("Authorization", "Bearer " + token);
+
+            UnityWebRequestAsyncOperation asyncOp = webRequest.SendWebRequest();
+            while (!asyncOp.isDone)
+            {
+                await Task.Yield();
+                if (onProgress != null)
+                    onProgress.Invoke(asyncOp.progress);
+            }
+            if (onDone != null)
+                onDone.Invoke();
+
+            long responseCode = -1;
+            bool isHttpError = true;
+            bool isNetworkError = true;
+            string stringContent = string.Empty;
+            string error = string.Empty;
+
+            responseCode = webRequest.responseCode;
+#if UNITY_2020_2_OR_NEWER
+            isHttpError = (webRequest.result == UnityWebRequest.Result.ProtocolError);
+            isNetworkError = (webRequest.result == UnityWebRequest.Result.ConnectionError);
+#else
+            isHttpError = webRequest.isHttpError;
+            isNetworkError = webRequest.isNetworkError;
+#endif
+            if (!isNetworkError)
+                stringContent = webRequest.downloadHandler.text;
+            else
+                error = webRequest.error;
+
+            return new RestClient.Result(responseCode, isHttpError, isNetworkError, stringContent, error);
         }
     }
 }
